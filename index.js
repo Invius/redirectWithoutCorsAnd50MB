9d307676f0b0499444ed4583da75ae2738fb7942
Invius <marco.fernandes.inf10@hotmail.com>
2022-11-23 02:13:01 +0000

    enabled code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express";
-/* import dotenv from "dotenv";
+import dotenv from "dotenv";
 import cors from 'cors';
-import bodyParser from 'body-parser'; */
+import bodyParser from 'body-parser';
 
-//dotenv.config();
+dotenv.config();
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 const port = 80;
 
-/* app.use(express.raw({
+app.use(express.raw({
   inflate: true,
   limit: '50mb',
   type: () => true, // this matches all content types
@@ -28,13 +28,6 @@ app.post("*", function (req, res) {
   //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
 
   res.redirect(307, `${process.env.API_TO_REDIRECT}${req.url}`);
-});*/
-
-app.get("/", function (req, res) {
-  //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
-
-  //res.redirect(`${process.env.API_TO_REDIRECT}${req.url}`);
-  res.send("Hello World!");
 });
 
 app.listen(port, () => {
