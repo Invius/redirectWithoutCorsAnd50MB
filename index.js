4fb861e49f62dab98db4c9fc6108b9e8bc591af7
Invius <marco.fernandes.inf10@hotmail.com>
2022-11-23 00:32:31 +0000

    only push needed

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const port = 1670;
+
+app.use(express.raw({
+  inflate: true,
+  limit: '50mb',
+  type: () => true, // this matches all content types
+}));
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.get("*", function (req, res) {
+  //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
+
+  res.redirect(`${process.env.API_TO_REDIRECT}${req.url}`);
+});
+
+app.post("*", function (req, res) {
+  //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
+
+  res.redirect(307, `${process.env.API_TO_REDIRECT}${req.url}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
