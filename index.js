c22ba4d0cda9e9f2e2f22630374a3942fd299431
Invius <marco.fernandes.inf10@hotmail.com>
2022-11-23 22:28:39 +0000

    axios redirect final

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ import axios from 'axios';
 dotenv.config();
 
 const app = express();
+const port = 80;
+const commonHeaders = {
+  "Access-Control-Allow-Origin": '*',
+}
+const configAxios = {
+  headers: commonHeaders,
+};
+
 app.use(cors());
 
-const port = 80;
 
 app.use(express.raw({
   inflate: true,
@@ -19,25 +26,49 @@ app.use(express.raw({
 
 app.use(bodyParser.json({limit: '50mb'})); 
 
-/* app.get("*", function (req, res) {
-  //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
-
-  res.redirect(`${process.env.API_TO_REDIRECT}${req.url}`);
-});
-
+/* 
+redirect example
 app.post("*", function (req, res) {
   //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
 
   res.redirect(307, `${process.env.API_TO_REDIRECT}${req.url}`);
-}); */
+}); 
+*/
 
-app.get("/", function (req, res) {
-  //res.send(req.url + " : " + process.env.API_TO_REDIRECT);
+app.get("*", function (req, res) {
 
-  axios.get(`${process.env.API_TO_REDIRECT}/`)
-  .then( (response) => {
-    res.send(response.data);
-  });
+  try {
+    axios.get(`${process.env.API_TO_REDIRECT}${req.url}`, configAxios)
+    .then( (response) => {
+      res.send(response.data);
+    }).catch((err)=>{
+      res.status(500);
+      res.send({error: "Internal Error"});
+    });
+  } catch (error) {
+    res.status(500);
+    res.send({error: "Internal Error"});
+  }
+
+});
+
+app.post("*", function (req, res) {
+
+  try {
+    const payload =  JSON.parse(req.body || {});
+
+    axios.post(`${process.env.API_TO_REDIRECT}${req.url}`,
+    payload, configAxios)
+    .then( (response) => {
+      res.send(response.data);
+    }).catch((err)=>{
+      res.status(500);
+      res.send({error: "Internal Error"});
+    });
+  } catch (error) {
+    res.status(500);
+    res.send({error: "Internal Error"});
+  }
 });
 
 app.listen(port, () => {
